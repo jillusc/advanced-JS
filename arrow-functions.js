59ba82d9177c5e0dcb654490a778d5e81b1316ee
jillusc <>
2023-11-15 12:29:59 +0000

    Notes on arrow functions preliminaries

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,58 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+
+let sum = addTwoNumbers(5, 3);
+console.log(sum)
+
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    return a + b;
+}
+
+let sum = addTwoNumbers(4, 3);
+console.log(sum)
+
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers = (a, b) => a + b;
+let sum = addTwoNumbers(4, 3);
+console.log(sum)
+
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello World!');
 
-// Returning Multiple Lines
+const sayHello = () => console.log('Hello');
+
+// Or
+const returnSingleLine = () => 'This is a single-line string';
+
+
+// Returning Multiple Lines - IMplicit
+const returnMultipleLines = () => (
+    `<p>
+      "This is a multiline string."
+    </p>`
+)
+// Returning Multiple Lines - EXplicit
+const returnMultipleLinesExplicit = () => {
+    return (
+      `<p>
+        "This is a multiline string."
+      </p>`
+    );
+  };
+
+
+
+
+
+
+
+
